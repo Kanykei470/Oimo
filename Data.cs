053202999155701d9bb2c6e549286d0f924303c4
Kanykei470 <akanykej73@gmail.com>
2022-11-23 14:38:58 +0600

    First commit

---
+++ Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oimo
+{
+    public class OutData
+    {
+        public string? token { get; set; }
+        public string account_number { get; set; }
+        public string? refill_date_time { get; set; }
+        public double? amount { get; set; }
+        public string action { get; set; }
+
+        public IEnumerator GetEnumerator() => this.GetEnumerator();
+    }
+
+    public class GetData
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+    }
+
+   
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Text.Json;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oimo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            //Проверка Get
+            //using (var client = new HttpClient())
+            //{
+            //    var url = new Uri("https://billing.crm.mycloud.kg/pay/main.php ");
+            //    var result = client.GetAsync(url).Result;
+            //    var json = result.Content.ReadAsStringAsync().Result;
+            //    Lbl.Content = json;
+            //}
+
+            string token = "yVKdAGWwCu6sb7j8CNjhiW7JeFv7CBlN";
+            string date = DateTime.Now.ToString();
+            double amount = 100;
+            string action = "pay";
+
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri("https://billing.crm.mycloud.kg/pay/main.php ");
+                var newPost = new OutData()
+                {
+                    token = token,
+                    account_number = txtBx.Text,
+                    refill_date_time = date,
+                    amount = amount,
+                    action = action
+                };
+
+                var newPostJson = JsonConvert.SerializeObject(newPost);
+                var payLoad = new StringContent(newPostJson, Encoding.UTF8, "application/json");
+                var result = client.PostAsync(uri, payLoad).Result.Content.ReadAsStringAsync().Result;
+                Lbl.Content = result;
+            }
+
+
+         
+
+        }
+
+    }
+}
